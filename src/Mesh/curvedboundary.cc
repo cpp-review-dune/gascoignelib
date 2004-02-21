d37f9695be4fa3bfa3ea436278fe39940d39ac03
roland <roland>
2004-02-21 20:13:41 +0000

    .

--- src/Mesh/curvedboundary.cc
+++
@@ -1,9 +0,0 @@
-#include  "curvedboundary.h"
-
-/*---------------------------------------------*/
-
-double SinusBoundary::operator()(const Vertex2d& c) const 
-{
-  double pi = 3.1415925359;
-  return -1.+0.25*sin(pi/2.*(c.x()+1.)) - c.y();
-}
