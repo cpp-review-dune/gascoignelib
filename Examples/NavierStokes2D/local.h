d6d01e91977606b83c6a5fac0469ffe04752d642
roland <roland>
2004-06-04 18:29:33 +0000

    .

--- Examples/NavierStokes2D/local.h
+++ Examples/NavierStokes2D/local.h
@@ -55,23 +55,20 @@ public:
 
 class RunderKreis : public BoundaryFunction<2>
 {
-  double squareradius;
-  Vertex2d center;
+  double   _r;
+  Vertex2d _c;
 public :
 
   std::string GetName() const { return "RunderKreis";}
-
-  void BasicInit(Vertex2d c, double r) 
-    {
-      center = c; 
-      squareradius = r;
+  void BasicInit(Vertex2d c, double r) {
+      _c = c; 
+      _r = r;
     }
-  double operator()(const Vertex2d& c) const 
-    {
-      double r = - squareradius;
+  double operator()(const Vertex2d& c) const {
+      double r = - _r;
       for (int i=0; i<2; i++)
 	{
-	  double dx = c[i]-center[i];
+	  double dx = c[i]-_c[i];
 	  r += dx * dx;
 	}
       return r;
--- src/Interface/neumanndata.h
+++ src/Interface/neumanndata.h
@@ -33,8 +33,8 @@ public:
 
   virtual std::string GetName() const=0;
   
-  virtual void operator()(Gascoigne::VectorIterator b, const Gascoigne::TestFunction& N, const Vertex2d& v, const Vertex2d& n, int col) const {assert(0);}
-  virtual void operator()(Gascoigne::VectorIterator b, const Gascoigne::TestFunction& N, const Vertex3d& v, const Vertex3d& n, int col) const {assert(0);}
+  virtual void operator()(Gascoigne::VectorIterator b, const Gascoigne::TestFunction& N, const Vertex2d& v, const Vertex2d& n, int color) const {assert(0);}
+  virtual void operator()(Gascoigne::VectorIterator b, const Gascoigne::TestFunction& N, const Vertex3d& v, const Vertex3d& n, int color) const {assert(0);}
 };
 
 
