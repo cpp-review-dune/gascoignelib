42f3307dfa9a7d43c2e55ad9bc040c8ef6632840
roland <roland>
2005-02-24 11:04:59 +0000

    Teil 2

--- src/Solver/newtonstdmultilevelsolver.cc
+++
@@ -1,8 +0,0 @@
-#include  "newton.xx"
-#include  "stdmultilevelsolver.h"
-#include  "multilevelghostvector.h"
-
-namespace Gascoigne
-{
-template void newton(StdMultiLevelSolver& S, MultiLevelGhostVector& u, const MultiLevelGhostVector& f, MultiLevelGhostVector& r, MultiLevelGhostVector& w, NLInfo& info);
-}
