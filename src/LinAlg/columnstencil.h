855d265b09b2e73cf8ae0ae9d716ffa1caaec7cb
richter <richter>
2005-08-31 07:51:25 +0000

    RangeErrorStartStop war auf diese Art falsch und sinnvoll, ist deshalb rausgeflogen

--- src/LinAlg/columnstencil.h
+++ src/LinAlg/columnstencil.h
@@ -18,25 +18,20 @@ namespace Gascoigne
 ////
 /////////////////////////////////////////////
 
-class ColumnStencil : public virtual StencilInterface
+  class ColumnStencil //: public StencilInterface
 {
 protected:
 
   IntVector   scol, sstart;
-
-  void _RangeErrorStartStop(int i, const std::vector<int>& vec) const 
-  {
-    assert(i>=0);
-    assert(i<=sstart.size());
-  }  
-
+  
 public:
 
 //
 ////  Con(De)structor 
 //
 
-  ColumnStencil() : StencilInterface() {}
+  ColumnStencil() //: StencilInterface()
+    {}
   ~ColumnStencil() {}
 
   const IntVector&  col()    const { return scol; }
@@ -46,14 +41,14 @@ public:
 
   int  n()            const { return sstart.size()-1;}
   int  nentries()     const { return scol.size();}
-  int  rowsize(int i) const { return sstart[i+1]-sstart[i];}
+  int  rowsize(int i) const { assert((i>=0)&&(i+1<sstart.size())); return sstart[i+1]-sstart[i];}
 
         int&  col(int pos)           { assert((pos>=0)&&(pos<scol.size())); return scol[pos]; } 
   const int&  col(int pos)     const { assert((pos>=0)&&(pos<scol.size())); return scol[pos]; } 
-        int&  start(int i)           {  _RangeErrorStartStop(i,sstart); return sstart[i]; } 
-  const int&  start(int i)     const {  _RangeErrorStartStop(i,sstart); return sstart[i]; } 
-        int&  stop(int i)            {  _RangeErrorStartStop(i,sstart); return sstart[i+1]; } 
-  const int&  stop(int i)      const {  _RangeErrorStartStop(i,sstart); return sstart[i+1]; } 
+        int&  start(int i)           { assert((i>=0)&&(i<sstart.size()));   return sstart[i]; } 
+  const int&  start(int i)     const { assert((i>=0)&&(i<sstart.size()));   return sstart[i]; } 
+        int&  stop(int i)            { assert((i>=0)&&(i+1<sstart.size())); return sstart[i+1]; } 
+  const int&  stop(int i)      const { assert((i>=0)&&(i+1<sstart.size())); return sstart[i+1]; } 
 
   void memory(const SparseStructureInterface*);
   void memory(int n, int nt);
