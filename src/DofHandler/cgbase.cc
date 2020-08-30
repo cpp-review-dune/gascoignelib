1f33c0492b6e58950191c0b5bc1fe5ebc922e67b
Thomas Richter <thomas.richter@ovgu.de>
2020-08-30 09:08:15 +0200

    .

---
+++ src/DofHandler/cgbase.cc
@@ -0,0 +1,8 @@
+#include "cgbase.h"
+
+namespace Gascoigne
+{
+  template class CGBase<2,2>;
+  template class CGBase<2,3>;
+  template class CGBase<2,5>;
+}
---
+++ src/DofHandler/lagrangedisc.cc
@@ -0,0 +1,34 @@
+//#include "cgdisc.xx"
+#include "lagrangedisc.h"
+
+#include "finiteelement.h"
+#include "elementintegrator.h"
+#include "elementlpsintegrator.h"
+#include "integrationformula.h"
+#include "transformation2d.h"
+#include "transformation3d.h"
+#include "baseq12d.h"
+#include "baseq13d.h"
+#include "baseq22d.h"
+#include "baseq23d.h"
+
+#include "baseq1patch.h"
+#include "baseq13dpatch.h"
+
+#include "patchintegrationformula.h"
+#include "finiteelement.xx"
+
+#include "compose_name.h"
+#include "cgbase.h"
+
+namespace Gascoigne
+{
+  template class LagrangeDiscQ12d;
+  template class LagrangeDiscQ22d;
+  template class LagrangeDiscQ42d;
+
+  template class LagrangeDiscQ13d;
+  template class LagrangeDiscQ23d;
+  template class LagrangeDiscQ43d;
+}
+
